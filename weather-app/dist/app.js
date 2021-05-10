0654ac9db855b2f292ef0997a6cb1d69be2328ab
Touhidul Shawan <touhidulshawan@gmail.com>
2021-05-10 14:09:23 +0600

    setup for weather app

---
+++ weather-app/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("App Starting");
+setTimeout(function () {
+    console.log("4 second later");
+}, 4000);
+console.log("App Stopping");
+//# sourceMappingURL=app.js.map
---
+++ weather-app/src/app.ts
@@ -0,0 +1,7 @@
+console.log("App Starting");
+
+setTimeout(() => {
+  console.log("4 second later");
+}, 4000);
+
+console.log("App Stopping");
