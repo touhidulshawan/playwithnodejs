b7889edf3a10a0aa3e22ca3678bcbfc735737ac9
Touhidul Shawan <touhidulshawan@gmail.com>
2021-05-10 16:19:42 +0600

    request forecast of weather using the weather api

--- weather-app/src/app.ts
+++ weather-app/src/app.ts
@@ -1,7 +1,17 @@
-console.log("App Starting");
+require("dotenv").config();
+import axios from "axios";
 
-setTimeout(() => {
-  console.log("4 second later");
-}, 4000);
+const lon = 77.4977;
+const lat = 27.2046;
 
-console.log("App Stopping");
+const getWeatherData = async () => {
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`
+  );
+  const data = await response.data;
+  console.log(
+    `${data.daily[0].weather[0].description}. It is currently ${data.current.temp} degrees out there. There is ${data.current.clouds}% chance of raining.`
+  );
+};
+
+getWeatherData();
