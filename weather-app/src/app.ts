4a8f53ae750c9d4594a0245f4e2030e99513651a
Touhidul Shawan <touhidulshawan@gmail.com>
2021-05-11 14:57:34 +0600

    simple error handling

--- weather-app/src/app.ts
+++ weather-app/src/app.ts
@@ -5,13 +5,17 @@ const lon = 77.4977;
 const lat = 27.2046;
 
 const getWeatherData = async () => {
-  const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`
-  );
-  const data = await response.data;
-  console.log(
-    `${data.daily[0].weather[0].description}. It is currently ${data.current.temp} degrees out there. There is ${data.current.clouds}% chance of raining.`
-  );
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`
+    );
+    const data = await response.data;
+    console.log(
+      `${data.daily[0].weather[0].description}. It is currently ${data.current.temp} degrees out there. There is ${data.current.clouds}% chance of raining.`
+    );
+  } catch (err) {
+    console.log("Something Wrong!! Try again later.");
+  }
 };
 
 getWeatherData();
