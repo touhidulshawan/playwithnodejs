d7810a084e8118d807e5d1e07161cf4d8c0d841b
Touhidul Shawan <touhidulshawan@gmail.com>
2021-05-17 22:50:11 +0600

    serving static files like [images, css] with express

--- web-server[express]/src/app.ts
+++ web-server[express]/src/app.ts
@@ -5,23 +5,23 @@ const app = express();
 
 const publicFolderDirectory = path.join(__dirname, "../public");
 
+// root path - load index.html file when web page serve
 app.use(express.static(publicFolderDirectory));
 
-// root path
-app.get("/", (req, res) => {
-  res.send();
-});
+//! Do need now cause this three pages is serving through html file with [above] line
+// // root
+// app.get("/", (req, res)=> {
+//   res.send("<p>This is home page</p>")
+// })
+// // about page
+// app.get("/about", (req, res) => {
+//   res.send("<p>This is about page</p>");
+// });
 
-// about page
-
-app.get("/about", (req, res) => {
-  res.send("<p>This is about page</p>");
-});
-
-// service page
-app.get("/service", (req, res) => {
-  res.send("<p>These are our services</p>");
-});
+// // service page
+// app.get("/service", (req, res) => {
+//   res.send("<p>These are our services</p>");
+// });
 
 app.listen("3000", () => {
   console.log("Server is running on port 3000");
