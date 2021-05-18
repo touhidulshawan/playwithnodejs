c53a45ee3e6ef40d1f524408f2bed00ab6a93c5c
Touhidul Shawan <touhidulshawan@gmail.com>
2021-05-18 13:14:15 +0600

    render page with hbs

--- web-server[express]/src/app.ts
+++ web-server[express]/src/app.ts
@@ -3,26 +3,26 @@ import express from "express";
 
 const app = express();
 
+// set view engine
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "hbs");
+
 const publicFolderDirectory = path.join(__dirname, "../public");
 
 // root path - load index.html file when web page serve
 app.use(express.static(publicFolderDirectory));
 
-//! Do need now cause this three pages is serving through html file with [above] line
-// // root
-// app.get("/", (req, res)=> {
-//   res.send("<p>This is home page</p>")
-// })
-// // about page
-// app.get("/about", (req, res) => {
-//   res.send("<p>This is about page</p>");
-// });
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-// // service page
-// app.get("/service", (req, res) => {
-//   res.send("<p>These are our services</p>");
-// });
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
+app.get("/service", (req, res) => {
+  res.render("service");
+});
 app.listen("3000", () => {
   console.log("Server is running on port 3000");
 });
