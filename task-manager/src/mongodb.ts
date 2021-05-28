28c84f6515bb2c111fd8f1c87ca2a48aed6f932f
Touhidul Shawan <touhidulshawan@gmail.com>
2021-05-28 07:53:08 +0600

    insert multiple data to users collection

--- task-manager/src/mongodb.ts
+++ task-manager/src/mongodb.ts
@@ -11,13 +11,36 @@ MongoClient.connect(
     if (error) return console.log(error);
 
     const db = client.db(databaseName);
-    db.collection("users").insertOne(
-      {
-        name: "Shawan",
-        age: 25,
-      },
+    // insert single data to users collection
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Shawan",
+    //     age: 25,
+    //   },
+    //   (error, result) => {
+    //     if (error) return console.log("Unable to insert into user");
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // insert multiple data to users collection
+    db.collection("users").insertMany(
+      [
+        {
+          name: "Shrabon",
+          age: 24,
+        },
+        {
+          name: "Mike",
+          age: 28,
+        },
+        {
+          name: "John",
+          age: 32,
+        },
+      ],
       (error, result) => {
-        if (error) return console.log("Unable to insert into user");
+        if (error) return console.log("unable to insert users");
         console.log(result.ops);
       }
     );
