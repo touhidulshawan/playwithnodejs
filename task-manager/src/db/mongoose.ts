183bb6cb1da04d5b1b66bdeea5ceb0406e5f88f1
Touhidul Shawan <touhidulshawan@gmail.com>
2021-05-29 15:51:40 +0600

    create db and insert collection with document using mongoose

--- task-manager/src/db/mongoose.ts
+++ task-manager/src/db/mongoose.ts
@@ -1,27 +1,55 @@
-import { connect, model, Schema, Model, Document } from "mongoose";
+import { connect, Document, model, Model, Schema } from "mongoose";
 
 interface IUser extends Document {
   name: string;
   age: number;
 }
 
-const username = "touhidulshawan";
-const password = "shawan96";
+// TODO: CREATE A CONNECTION
+const connectionURL = "mongodb://127.0.0.1:27017/task-manager-api";
 
-const connectionURL = `mongodb://${username}:${password}@127.0.0.1:27017/task-manager-api`;
+const connectToDB = async () => {
+  try {
+    await connect(connectionURL, {
+      user: "touhidulshawan",
+      pass: "shawan96",
+      authSource: "admin",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to database :)");
+  } catch (err) {
+    console.log(err.stack);
+    process.exit(1);
+  }
+};
 
-connect(connectionURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true,
-  useCreateIndex: true,
-});
+// TODO: CREATE A SCHEMA OF USER
 
-// create a User Schema
 const UserSchema: Schema = new Schema({
   name: { type: String, required: true },
   age: { type: Number, required: true },
 });
+// TODO: CREATE A MODEL OF USER
 
-// create a User Model
 const User: Model<IUser> = model("User", UserSchema);
+
+// TODO: CREATE A INSTANCE OF USER MODEL AND INSERT DATA AND SAVE DATA
+const insertUser = async () => {
+  try {
+    const user: IUser = await User.create({
+      name: "Touhidul Shawan",
+      age: 25,
+    });
+
+    await user.save();
+    console.log("Data inserted successfully");
+  } catch (err) {
+    console.log("Can not insert data to database");
+  }
+};
+
+connectToDB();
+insertUser();
